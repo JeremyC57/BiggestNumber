b2bccdb3233855efe3655a35ad8afb75fe33380e
JeremyC57 <jcasadaelliott@mail.csuchico.edu>
2019-09-11 16:49:52 -0700

    Now Outputs whichNumber is Biggest

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -6,6 +6,7 @@ int main()
   //declare variables here
   int firstNum = 0;
   int secondNum = 0;
+  int biggestNum = 0;
 
 
   std::cout<<"Please enter a whole number:\n";
@@ -22,9 +23,16 @@ int main()
  
   
   //determine if first or second is bigger and store that in a third variable
+  if( firstNum >secondNum)
+  {
+        biggestNum = firstNum;
+  }
+  else
+  {
+          biggestNum = secondNum;
+  }
 
-
-  std::cout<<"Of those two numbers, the biggest is: ";
+  std::cout<<"Of those two numbers, the biggest is: "<< biggestNum ;
   //print out the variable representing the biggest number
 
   std::cout<<std::endl<<"Thank you for playing.\n";
